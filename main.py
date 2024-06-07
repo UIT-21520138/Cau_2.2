168af26c429b6bf1cbeaa8d449641937881467e5
UIT.21520138 <21520138@gm.uit.edu.vn>
2024-06-07 10:50:22 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import time
+import webbrowser as wb
+
+st.title('Đăng nhập')
+username = st.text_input('Username', '')
+password = st.text_input('Password', '', type='password')
+if st.button('Đăng nhập'):
+    if username == '21520138' and password == '23102003':
+        st.success('Đăng nhập thành công')
+        time.sleep(3)
+        wb.open('https://www.uit.edu.vn/')
+    else:
+        st.error('Sai username hoặc password')
