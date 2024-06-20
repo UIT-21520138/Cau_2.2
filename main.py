24169f9858410d1b85c01380595761aac30b3622
UIT.21520138 <21520138@gm.uit.edu.vn>
2024-06-20 18:25:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import webbrowser as wb
 st.title('Đăng nhập')
 username = st.text_input('Username', '')
 password = st.text_input('Password', '', type='password')
-
 if st.button('Đăng nhập'):
     if username == '21520138' and password == '23102003':
         st.success('Đăng nhập thành công')
         time.sleep(3)
-        wb.open('https://courses.uit.edu.vn/')
+        wb.open('https://www.uit.edu.vn/')
     else:
         st.error('Sai username hoặc password')
